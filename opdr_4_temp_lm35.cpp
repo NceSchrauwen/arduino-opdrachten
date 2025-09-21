ff61713dbc89b6a25cde829a9d682716dcdef8e3
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-09-21 16:13:19 +0200

    Update opdr_4_temp_lm35.cpp
    Commentaar verandert naar NL.

--- opdr_4_temp_lm35.cpp
+++ opdr_4_temp_lm35.cpp
@@ -1,30 +1,29 @@
-// Opdracht 4 - Detect the temperature and convert raw ADC value to temp in Celcius (lm35 used)
-// Written by: Nina Schrauwen
-// Bron: https://lastminuteengineers.com/tmp36-temperature-sensor-arduino-tutorial/ ( https://arduinopoint.com/lm35-arduino-project/ )
+// Opdracht 4 - Meet de temperatuur en zet de ruwe ADC-waarde om naar °C (LM35)
+// Geschreven door: Nina Schrauwen
+// Bron: https://deepbluembedded.com/arduino-lm35-temperature-sensor-interfacing/
 
-// Define constant variables
-const int TEMP = A0;
+const int TEMP = A0; // pin waar de sensor op zit
 
 void setup()
 {
-  //Begin serial communication to be able to track console output
+  // Start seriële communicatie om waarden te kunnen volgen in de monitor
   Serial.begin(9600);
 }
 
 void loop()
 {
-  int adcValue = analogRead(TEMP); //raw adc value
-  float voltage = (adcValue / 1023.0) * 5.0; // converting adc to voltage 
-  
-  //converting voltage into degree Celcius (subtracts the 500mV offset of the TMP36 and the 100 multiplies to convert volts to millivolts)
-  float tempC = voltage * 100;  // LM35 Calculation
+  int adcValue = analogRead(TEMP);  // lees de rauwe ADC-waarde
+  float voltage = ((adcValue * 5.0) / 1023.0) * 1000;  
+  // zet ADC-waarde om naar spanning → nodig om temperatuur te kunnen berekenen
 
-  // Debug print to keep track of the temp in the serial monitor
+  float tempC = voltage / 10;  
+  // LM35 geeft 10mV per graad, dus spanning delen door 10 = temperatuur in °C
+
+  // Print temperatuur in seriële monitor
   Serial.print("Temp is: ");
   Serial.print(tempC);
-  Serial.print("\xB0"); // Degree symbol
+  Serial.print("\xB0");
   Serial.println("C");
   
-  // Delay so you don't have constant temp readings
-  delay(5000);
+  delay(1000); // korte pauze zodat waarden niet constant voorbij vliegen
 }
