80e24dcd46152aa4fe73cc666f057ef44db3317b
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-09-22 15:35:23 +0200

    Update opdr_5_rgb.cpp

--- opdr_5_rgb.cpp
+++ opdr_5_rgb.cpp
@@ -1,21 +1,22 @@
-// Opdracht 5 - Code to cycle through all the possible rgb colors of the rgb led, done in steps of five to speed the process up a bit
-// Written by: Nina Schrauwen 
+// Opdracht 5 - Alle mogelijke RGB-kleuren doorlopen met de RGB-led
+// Gaat in stappen van 5 om het proces wat sneller te maken
+// Geschreven door: Nina Schrauwen
 // Bron: https://projecthub.arduino.cc/semsemharaz/interfacing-rgb-led-with-arduino-b59902
 
-// Define constant variables
+// Pins voor de RGB-led
 const int RED_P = 2;
 const int BLUE_P = 3;
 const int GREEN_P = 4;
-// Define global variables
+
+// Kleurwaardes (0–255)
 int red;
 int green;
 int blue;
 
 void setup()
 {
-  // Make a connection to the serial monitor to be able to produce console output
-  Serial.begin(9600);
-  // Define inputs to the corresponding pins
+  Serial.begin(9600); // verbinding met seriële monitor
+  
   pinMode(RED_P, OUTPUT);
   pinMode(BLUE_P, OUTPUT);
   pinMode(GREEN_P, OUTPUT);
@@ -23,30 +24,28 @@ void setup()
 
 void loop()
 {
-  // The blue will start then will move through all possibel green/blue color variations and then all red/green/blue combinations. (Will take some time even in steps of 5)
-  // To cycle through all the red values
-  for(red = 0; red <= 255; red += 5){
-	// To cycle through all the green values
-    for(green = 0; green <= 255; green += 5){
-	  // To cycle through all the green values
-      for(blue = 0; blue <= 255; blue += 5){
-      	setColor(red, green, blue);
+  // Alle mogelijke combinaties van rood, groen en blauw doorlopen
+  // Dit duurt even, ook al doen we stappen van 5
+  for (red = 0; red <= 255; red += 5) {
+    for (green = 0; green <= 255; green += 5) {
+      for (blue = 0; blue <= 255; blue += 5) {
+        setColor(red, green, blue);
       }
     }
   }
 }
 
-// Function to set the color according to the passed color values 
+// Functie om de RGB-led in te stellen op de gekozen waardes
 void setColor(int red, int green, int blue) {
-  //Debug print to keep track of the process of changing the values because it takes a long time to get through all the possible values
-  Serial.print("Setting Colors - Red: ");
+  // Debug: laat de huidige kleurwaardes zien
+  Serial.print("Kleur instellen - Rood: ");
   Serial.print(red);
-  Serial.print(", Green: ");
+  Serial.print(", Groen: ");
   Serial.print(green);
-  Serial.print(", Blue: ");
+  Serial.print(", Blauw: ");
   Serial.println(blue);
   
-  //Assign the color value to the corresponding color pin to turn on the right color(s) in the right value(s)
+  // Schrijf de waardes naar de juiste pinnen
   analogWrite(RED_P, red);
   analogWrite(GREEN_P, green);
   analogWrite(BLUE_P, blue);
