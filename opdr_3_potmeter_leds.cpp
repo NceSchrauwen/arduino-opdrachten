528b60eef89a0948d701d4436bb2991eb4853aa5
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-09-22 15:38:15 +0200

    Update opdr_3_potmeter_leds.cpp

--- opdr_3_potmeter_leds.cpp
+++ opdr_3_potmeter_leds.cpp
@@ -1,43 +1,42 @@
-// Opdracht 3 - Code to gradually turn on all the leds while turning the potentiometer
-// Written by: Nina Schrauwen
+// Opdracht 3 - 7 LEDs geleidelijk aansturen met een potentiometer
+// Geschreven door: Nina Schrauwen
 
-// Define constant variables
-const int LEDPINS[]= {2, 3, 4, 5, 6, 7, 8};
-int pot = A5; // Has to be tied to an analog value
+// LED-pinnen
+const int LEDPINS[] = {2, 3, 4, 5, 6, 7, 8};
+int pot = A5; // potentiometer moet analoog zijn
 
 void setup()
 {	
-  // Setting up a serial monitor to be able to monitor console output
-  Serial.begin(9600);
-  // Loop through and define the outputs of the ledpins
-  for(int i = 0; i < 7; i++){
-  	pinMode(LEDPINS[i], OUTPUT);
+  Serial.begin(9600); // seriële monitor starten
+  
+  // Alle LED-pinnen als output instellen
+  for (int i = 0; i < 7; i++) {
+    pinMode(LEDPINS[i], OUTPUT);
   }
 }
 
 void loop()
 {
-  // Read the value of the potentiometer
+  // Lees waarde van de potentiometer (0–1023)
   int potValue = analogRead(pot);
-  // Determine how many leds should be on based on the value of the potentiometer
+
+  // Bereken hoeveel LEDs aan moeten staan
   int numLedsOn = map(potValue, 0, 1023, 0, 7);
   
-  // Debug printing to track the potentiometer value
-  Serial.print("Potentiometer Value: ");
+  // Debug: laat potmeterwaarde en aantal LEDs zien
+  Serial.print("Potmeter: ");
   Serial.print(potValue);
-  Serial.print(" | LEDs On: ");
+  Serial.print(" | LEDs aan: ");
   Serial.println(numLedsOn);
   
-  // Loop through all the ledpins
+  // LEDs aan/uit zetten afhankelijk van de potmeterstand
   for (int i = 0; i < 7; i++) {
-      // If there are fewer leds on then calculated then turn those leds on
-      if (i < numLedsOn) {
-        digitalWrite(LEDPINS[i], HIGH);
-      // If there are too many leds on, turn those leds off
-      } else {
-        digitalWrite(LEDPINS[i], LOW);
-      }
+    if (i < numLedsOn) {
+      digitalWrite(LEDPINS[i], HIGH);
+    } else {
+      digitalWrite(LEDPINS[i], LOW);
     }
+  }
   
-  delay(100); // Small delay for stability
+  delay(100); // korte pauze voor stabiliteit
 }
