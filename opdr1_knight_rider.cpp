bb3595fe0134ff40b7284393b8ed5a6e5d336510
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-09-22 15:42:30 +0200

    Update opdr1_knight_rider.cpp

--- opdr1_knight_rider.cpp
+++ opdr1_knight_rider.cpp
@@ -1,28 +1,24 @@
-// Opdracht 1 - Blinking leds sequence like knight rider car lights
-// Written by: Nina Schrauwen
+// Opdracht 1 - Knight Rider lichtsequentie (LEDs heen en weer laten lopen)
+// Geschreven door: Nina Schrauwen
 
-// Define constant variables
+// LED-pinnen
 const int LEDS[] = {2, 3, 4, 5, 6, 7, 8, 9};
-
 
 void setup()
 {	
-  // Define the ledpin outputs by looping through each of them
-  for(int i = 0; i <= 7; i++){
+  // Alle LED-pinnen als output instellen
+  for (int i = 0; i <= 7; i++) {
     pinMode(LEDS[i], OUTPUT);
   }
 }
 
-//Function to light up the leds from left to right 
-void to(){
-  // Loop through all ledpins
-  for(int i = 0; i < 7; i++){
-    // If it has reached the end don't turn it off 
-	// If it would go off at the end and then on again the other way around it would blink twice at the end which would not be a smooth transition
-    if(i == 7){
+// LEDs van links naar rechts laten lopen
+void to() {
+  for (int i = 0; i < 7; i++) {
+    // Uiteinde → LED blijft even aan voor vloeiende overgang
+    if (i == 7) {
       digitalWrite(LEDS[i], HIGH);
       delay(75);
-    // If it has not reached the end turn it on and off 
     } else {
       digitalWrite(LEDS[i], HIGH);
       delay(75);
@@ -32,15 +28,13 @@ void to(){
   }
 }
 
-//Function to light up the leds from right to left 
-void from(){
-  // If it has reached the end don't turn it off 
-  // If it would go off at the end and then on again the other way around it would blink twice at the end which would not be a smooth transition
-  for(int i = 7; i >= 0; i--){
-    if(i == 0){
+// LEDs van rechts naar links laten lopen
+void from() {
+  for (int i = 7; i >= 0; i--) {
+    // Uiteinde → LED blijft even aan
+    if (i == 0) {
       digitalWrite(LEDS[i], HIGH);
       delay(75);
-    // It has come either from the end or is somewhere in the middle so just turn on and off
     } else {
       digitalWrite(LEDS[i], HIGH);
       delay(75);
@@ -52,7 +46,7 @@ void from(){
 
 void loop()
 {
-  // Call the functions within the loop so it can move back and forth on repeat like the car lights in knight rider
+  // Wissel heen en weer voor het Knight Rider-effect
   to();
   from();
 }
